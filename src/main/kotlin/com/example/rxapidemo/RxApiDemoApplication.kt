aa68dbe47d9f9115b44192dcf0643dcacf2403d2
Michal Koszalka <michalko@practi.net>
2022-05-25 22:37:08 +0200

    Init

---
+++ src/main/kotlin/com/example/rxapidemo/RxApiDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rxapidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RxApiDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<RxApiDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/rxapidemo/RxApiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.rxapidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RxApiDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
